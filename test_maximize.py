87fae244fe3f1f77314ad8f1100056b35db740d4
Nazarius <nazar.sozanskiy@gmail.com>
2023-11-06 02:49:45 +0200

    initial commit

--- test_maximize.py
+++ test_maximize.py
@@ -1,14 +1,12 @@
-import unittest
 import maximize
 
 
-class TestMain(unittest.TestCase):
+def test_f():
+    assert maximize.f(0) == 0
 
-    def test_f(self):
-        assert maximize.f(0) == 0
 
-    def test_maximize(self):
-        assert maximize.maximize(maximize.f, maximize.a, maximize.b, maximize.epsilon)[1] > 0
-        assert maximize.f(maximize.maximize(maximize.f, maximize.a, maximize.b, maximize.epsilon)[0]) >= maximize.f((maximize.a + maximize.b) / 2)
-        assert maximize.maximize(maximize.f, maximize.a, maximize.b, maximize.epsilon)[0] <= maximize.b
-        assert maximize.maximize(maximize.f, maximize.a, maximize.b, maximize.epsilon)[0] >= maximize.a
+def test_maximize():
+    assert maximize.maximize(maximize.f, maximize.a, maximize.b, maximize.epsilon)[1] > 0
+    assert maximize.f(maximize.maximize(maximize.f, maximize.a, maximize.b, maximize.epsilon)[0]) >= maximize.f((maximize.a + maximize.b) / 2)
+    assert maximize.maximize(maximize.f, maximize.a, maximize.b, maximize.epsilon)[0] <= maximize.b
+    assert maximize.maximize(maximize.f, maximize.a, maximize.b, maximize.epsilon)[0] >= maximize.a
